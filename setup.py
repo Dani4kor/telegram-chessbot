858cd938dafc1e829957e479158d01666ca40cc9
Dani4kor <dani4kor@gmail.com>
2016-11-22 01:22:50 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='telechessbot',
+    version='0.0.1',
+    packages=['src'],
+    url='https://github.com/Dani4kor/telegram-chessbot',
+    license='GPLv3 ',
+    author='Dani',
+    author_email='dani4kor@gmail.com',
+    description='src @chessaybot telegrambot '
+)
