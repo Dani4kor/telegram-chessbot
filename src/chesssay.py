5977e82eb3cead287dff19c719ca39a481db1608
Dani4kor <dani4kor@gmail.com>
2016-11-22 02:27:18 +0200

    add REGEX control of FEN position

--- src/chesssay.py
+++ src/chesssay.py
@@ -5,20 +5,26 @@ token = ''  # Telegram token
 bot = telebot.TeleBot(token)
 chessEngine = stockfishpy.Engine(
     'stockfishpath', param={'Threads': 2, 'Ponder': 'true'})
+chessEngine.uci()
 
 
 def call_stockfish_engine(message):
-    chessEngine.uci()
-    chessEngine.isready()
-    chessEngine.ucinewgame()
-    chessEngine.setposition(message)
-    move = chessEngine.bestmove()
-    return move['bestmove']
+    if chessEngine.isready() == 'readyok':
+	    chessEngine.ucinewgame()
+	    chessEngine.setposition(message)
+	    move = chessEngine.bestmove()
+	    return move['bestmove']
+	else: return 'Chess Engine not readyok'
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, call_stockfish_engine(message.text))
+     if re.match('\s*([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw-]\s(([a-hkqA-HKQ]{1,4})|(-))\s(([a-h][36])|(-))\s\d+\s\d+\s*', message.text):
+        move = call_stockfish_engine(message.text)
+        bot.send_message(message.chat.id, move)
+    else:
+        move = 'Put right positition'
+        bot.send_message(message.chat.id, move)
 
 
 if __name__ == '__main__':
