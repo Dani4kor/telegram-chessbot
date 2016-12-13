b72dd597a0a960e8b2686c957776ae7ad5a99ce9
Dani4kor <dani4kor@gmail.com>
2016-12-13 22:57:24 +0200

    add fen pars func

--- src/chesssay.py
+++ src/chesssay.py
@@ -5,16 +5,12 @@ import re
 token = ''  # Telegram token
 bot = telebot.TeleBot(token)
 chessEngine = stockfishpy.Engine(
-    'stockfishpath', param={'Threads': 2, 'Ponder': 'true'})
+    'stockfishPATH', param={'Threads': 2, 'Ponder': 'true'})
 chessEngine.uci()
 
 
-
-
 def fenPass(fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'):
-    """
-    """
-    regexMatch=re.match('\s*^(((?:[rnbqkpRNBQKP1-8]+\/){7})[rnbqkpRNBQKP1-8]+)\s([b|w])\s([K|Q|k|q]{1,4})\s(-|[a-h][1-8])\s(\d+\s\d+)$', fen)
+    regexMatch = re.match('\s*^(((?:[rnbqkpRNBQKP1-8]+\/){7})[rnbqkpRNBQKP1-8]+)\s([b|w])\s([K|Q|k|q]{1,4})\s(-|[a-h][1-8])\s(\d+\s\d+)$', fen)
     if  regexMatch:
         regexList = regexMatch.groups()
         fen = regexList[0].split("/")
@@ -51,21 +47,13 @@ def fenPass(fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'):
 
 def call_stockfish_engine(message):
     if chessEngine.isready() == 'readyok':
-	    chessEngine.ucinewgame()
-	    chessEngine.setposition(message)
-	    move = chessEngine.bestmove()
-	    return move['bestmove']
-	else: return 'Chess Engine not readyok'
+        chessEngine.ucinewgame()
+        chessEngine.setposition(message)
+        move = chessEngine.bestmove()
+        return move['bestmove']
+    else: return 'Chess Engine not readyok'
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):   
-     if re.match('\s*^(((?:[rnbqkpRNBQKP1-8]+\/){7})[rnbqkpRNBQKP1-8]+)\s([b|w])\s([K|Q|k|q]{1,4})\s(-|[a-h][1-8])\s(\d+\s\d+)$', message.text):
-        move = call_stockfish_engine(message.text)
-        bot.send_message(message.chat.id, move)
-    else:
-        move = 'Put right positition'
-        bot.send_message(message.chat.id, move)
 
 
 if __name__ == '__main__':
